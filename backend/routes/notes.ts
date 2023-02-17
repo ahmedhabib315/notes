a15f2ad03c773ccb170c2be653af24fd5f4e225a
Muhammad Ahmed <ahmed.habib315@gmail.com>
2023-02-17 17:11:31 +0500

    add update and delete api

--- backend/routes/notes.ts
+++ backend/routes/notes.ts
@@ -5,7 +5,7 @@ import { body, validationResult } from "express-validator";
 import { fetchUser } from "../middleware/fetch-user";
 import Notes = require("../models/Notes");
 
-//Create a User Using POST "/api/notes/create", Require Auth
+//Create a Note Using POST "/api/notes/create", Require Auth
 router.post(
   "/create",
   fetchUser,
@@ -27,6 +27,7 @@ router.post(
       const userData = await User.findById(userId).select("-password");
 
       const createdAt = new Date();
+      const lastUpdated = new Date();
 
       if (!userData) {
         return res
@@ -40,6 +41,7 @@ router.post(
         tags,
         user: userId,
         createdAt,
+        lastUpdated,
       });
 
       const savedNote = await note.save();
@@ -52,7 +54,7 @@ router.post(
   }
 );
 
-//Create a User Using POST "/api/notes/fetchnotes", Require Auth
+//Fetch All notes Using Get "/api/notes/fetchnotes", Require Auth
 router.get("/fetchnotes", fetchUser, async (req, res) => {
   //Set Errors if Wrong Values is Passed
   const errors = validationResult(req);
@@ -79,5 +81,129 @@ router.get("/fetchnotes", fetchUser, async (req, res) => {
     res.status(500).send("Some Internal Error Occured");
   }
 });
+
+//Update a Note Using PUT "/api/notes/update", Require Auth
+router.put(
+  "/update/:id",
+  fetchUser,
+  [
+    body("title", "Please Enter a Title").exists(),
+    body("description", "Please Enter a Description").exists(),
+  ],
+  async (req, res) => {
+    //Set Errors if Wrong Values is Passed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let { user } = req;
+
+    const { title, description, tags } = req.body;
+    const noteId = req.params.id;
+    try {
+      const userId = user.id;
+      const userData = await User.findById(userId).select("-password");
+
+      if (!userData) {
+        return res
+          .status(400)
+          .json({ error: "Please Login with Correct Credentials" });
+      }
+
+      const updatedData = {};
+
+      if (title) updatedData["title"] = title;
+
+      if (description) updatedData["description"] = description;
+
+      if (tags) updatedData["tags"] = tags;
+
+      updatedData["lastUpdated"] = new Date();
+
+      let note = await Notes.findById(noteId);
+
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+
+      if (note.user.toString() !== userId) {
+        return res
+          .json(400)
+          .json({ error: "Please Login with Correct Credentials" });
+      }
+
+      note = await Notes.findByIdAndUpdate(
+        noteId,
+        { $set: updatedData },
+        { new: true }
+      );
+      res.json(note);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Some Internal Error Occured");
+    }
+  }
+);
+
+//Delete a Note Using PUT "/api/notes/update", Require Auth
+router.delete(
+  "/delete/:id",
+  fetchUser,
+  [
+    body("title", "Please Enter a Title").exists(),
+    body("description", "Please Enter a Description").exists(),
+  ],
+  async (req, res) => {
+    //Set Errors if Wrong Values is Passed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let { user } = req;
+
+    const { title, description, tags } = req.body;
+    const noteId = req.params.id;
+    try {
+      const userId = user.id;
+      const userData = await User.findById(userId).select("-password");
+
+      if (!userData) {
+        return res
+          .status(400)
+          .json({ error: "Please Login with Correct Credentials" });
+      }
+
+      const updatedData = {};
+
+      if (title) updatedData["title"] = title;
+
+      if (description) updatedData["description"] = description;
+
+      if (tags) updatedData["tags"] = tags;
+
+      updatedData["lastUpdated"] = new Date();
+
+      let note = await Notes.findById(noteId);
+
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+
+      if (note.user.toString() !== userId) {
+        return res
+          .status(400)
+          .json({ error: "Please Login with Correct Credentials" });
+      }
+
+      note = await Notes.findByIdAndDelete(noteId);
+      res.send("Note Has Been Deleted");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Some Internal Error Occured");
+    }
+  }
+);
 
 export = router;
